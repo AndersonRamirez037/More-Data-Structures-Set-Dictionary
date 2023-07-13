b2228882f877eec635ba0f6d73d5a7c3c2aa1d5c
AndersonRamirez037 <andersongamer037@gmail.com>
2023-07-13 17:01:46 -0500

    10. Best score

--- 10-best_score.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-def best_score(a_dictionary):
-    if a_dictionary is None or len(a_dictionary) == 0:
-        return None 
-    max_score = float()
-    for i in a_dictionary:
-        if a_dictionary[i] > max_score:
-            max_score = a_dictionary[i]
-    return max_score

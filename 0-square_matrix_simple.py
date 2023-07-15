decff027b862c5d224df0bc8de0e6d55986aeeb0
AndersonRamirez037 <andersongamer037@gmail.com>
2023-07-15 08:53:02 -0500

    0. Squared simple

--- 0-square_matrix_simple.py
+++ 0-square_matrix_simple.py
@@ -1,3 +1,2 @@
 def square_matrix_simple(matrix=[]):
-    square_matrix = [list(map(lambda i: i * i, row)) for row in matrix]
-    return square_matrix
+    return [list(map(lambda i: i * i, row)) for row in matrix]

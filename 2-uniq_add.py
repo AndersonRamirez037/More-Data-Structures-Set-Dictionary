886f20bf9d30354509a80f116cb56f53e8ee8f20
AndersonRamirez037 <andersongamer037@gmail.com>
2023-07-15 09:04:11 -0500

    2. Unique addition

--- 2-uniq_add.py
+++ 2-uniq_add.py
@@ -1,9 +1,3 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    sum = 0
-    temp = 0
-    for i in my_list:
-        if i > temp:
-            temp = i
-            sum += temp
-    return sum
+    return sum(set(my_list))
